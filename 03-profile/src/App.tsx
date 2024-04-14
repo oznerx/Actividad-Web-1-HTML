122c1db9a1017f4bc08db21770037e92e7d3f22b
ozner <A01742377@tec.mx>
2024-04-13 23:15:38 -0600

    profile: here is a brief description about myself

--- 03-profile/src/App.tsx
+++ 03-profile/src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+
+      <header className='w-full flex justify-between items-center flex-row'>
+        <a href='/' className='py-10'><span className='text-blue-500 font-mono text-2xl'>Home</span></a>
+        <a href='#experience' className='py-10'><span className='text-blue-500 font-mono text-2xl'>Experience</span></a>
+        <a href='#contact' className='py-10'><span className='text-blue-500 font-mono text-2xl'>Contact</span></a>
+      </header>
+
+      <section className='px-10 w-full flex flex-col justify-center items-center bg'>
+          <h1 className='py-[220px] text-gray-500 font-mono text-4xl'> Hi, I'm ozner, software developer & data scientist. <br/>Trader & very interested in the finance sector.</h1>
+      </section>
+
+      <div className='flex flex-col mt-20' id='experience'>
+        <h1 className='px-10 py-10 text-blue-500 font-mono text-3xl'>Experience</h1>
+        <ul className='px-10 pb-20 text-gray-500 font-mono text-2xl'>
+          <li>Software Developer</li>
+          <li>Data Scientist</li>
+          <li>Portfolio Manager</li>
+          <li>Data Analyst</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <footer className='px-20 w-full flex justify-between items-center bg-gray-100 rounded-md flex-row mt-52 mb-5' id='contact'>
+        <a href='https://www.linkedin.com/in/ozner/' className='py-10'><span className='text-blue-500 font-mono text-3xl'>LinkedIn</span></a>
+        <a href='https://www.kaggle.com/justozner' className='py-10'><span className='text-blue-500 font-mono text-3xl'>Kaggle</span></a>
+        <a href='https://github.com/oznerx' className='py-10'><span className='text-blue-500 font-mono text-3xl'>Github</span></a>
+      </footer>
+
+    </div>
   )
 }
 
